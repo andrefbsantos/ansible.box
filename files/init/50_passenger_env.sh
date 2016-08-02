76eec98439f5a831741d1c72813b652dbd1402f6
André Santos <andrefilipebrazsantos@gmail.com>
2016-08-02 12:27:04 +0100

    fix enconding

--- files/init/50_passenger_env.sh
+++ files/init/50_passenger_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [[ -d "/etc/nginx/conf.d" ]]; then
+if [[ -d "/etc/nginx/conf.d" ]]; then
   echo "passenger_app_env '${PASSENGER_APP_ENV:-production}';" > /etc/nginx/conf.d/passenger_app_env.conf
 fi
